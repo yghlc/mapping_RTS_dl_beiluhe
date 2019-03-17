a3e4ac6cc7e2b673fda3ebaf62956ae227b10752
Lingcao Huang <huanglingcao@gmail.com>
2019-03-16 21:20:10 -0600

    all the stuff

---
+++ cp_to_dropbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dir="/Users/huanglingcao/Dropbox/Research/Conference or written Paper/03 mapping thaw slumps (ALD) in Beiluhe"
+
+name=mapping_RTS_dl_beiluhe
+
+cp -p ${name}.pdf "${dir}"/.
+cp -p ${name}.docx "${dir}"/.
---
+++ figures/cp_fig.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dir='/Users/huanglingcao/Dropbox/Research/Conference or written Paper/03 mapping thaw slumps (ALD) in Beiluhe/figures_mapping_RTS_dl_beiluhe' 
+
+#dir="/Users/huanglingcao/Dropbox/Research/Conference\ or\ written\ Paper/03\ mapping\ thaw\ slumps\ \(ALD\)\ in\ Beiluhe/figures_mapping_RTS_dl_beiluhe" 
+
+#cd  "${dir}"
+#pwd
+
+#cp -p "${dir}"/study_area_loc_RTS_photo_trim.jpg .
+
+#cp -p "${dir}"/flowchart_trim.jpg .
+
+#cp -p "${dir}"/top5_curves_trim.jpg .
+
+#cp -p "${dir}"/IoU_imgAug16_new_trim.jpg .
+
+#cp -p "${dir}"/whole_area_mapped_trim.jpg .
+
+#cp -p "${dir}"/zoom_in_mapped_polygons_trim.jpg .
+
+#cp -p "${dir}"/geometric_var_mapped_trim.jpg .
+
+cp -p "${dir}"/terrain_var_fig_mapped_trim.jpg .
