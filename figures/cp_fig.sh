5fa8d1cbc027d0112fe261d27084623a8b4bd836
Lingcao Huang <huanglingcao@gmail.com>
2019-08-30 21:05:21 +0800

    update figures

--- figures/cp_fig.sh
+++ figures/cp_fig.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-dir='/Users/huanglingcao/Dropbox/Research/Conference or written Paper/03 mapping thaw slumps (ALD) in Beiluhe/figures_mapping_RTS_dl_beiluhe' 
+#dir='/Users/huanglingcao/Dropbox/Research/Conference or written Paper/03 mapping thaw slumps (ALD) in Beiluhe/figures_mapping_RTS_dl_beiluhe' 
 
-#dir="/Users/huanglingcao/Dropbox/Research/Conference\ or\ written\ Paper/03\ mapping\ thaw\ slumps\ \(ALD\)\ in\ Beiluhe/figures_mapping_RTS_dl_beiluhe" 
+dir='/Users/huanglingcao/Dropbox/Research/Conference or written Paper/03 mapping thaw slumps (ALD) in Beiluhe/figures_mapping_RTS_dl_beiluhe_revised'
 
 #cd  "${dir}"
 #pwd
 
 #cp -p "${dir}"/study_area_loc_RTS_photo_trim.jpg .
 
-cp -p "${dir}"/study_area_loc_RTS_photo_v2_trim.jpg .
+#cp -p "${dir}"/study_area_loc_RTS_photo_v2_trim.jpg .
 
 #cp -p "${dir}"/flowchart_trim.jpg .
 
 #cp -p "${dir}"/top5_curves_trim.jpg .
 
 #cp -p "${dir}"/IoU_imgAug16_label_trim.jpg .
+#cp -p "${dir}"/IoU_imgAug22_label_trim.jpg .
 
 #cp -p "${dir}"/whole_area_mapped_trim.jpg .
 
@@ -23,4 +24,4 @@ cp -p "${dir}"/study_area_loc_RTS_photo_v2_trim.jpg .
 
 #cp -p "${dir}"/geometric_var_mapped_trim.jpg .
 
-#cp -p "${dir}"/terrain_var_fig_mapped_trim.jpg .
+cp -p "${dir}"/terrain_var_fig_mapped_trim.jpg .
